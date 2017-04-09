d78fbe50b1f41b807463dde69fb663deea873398
slayercao <doomcao@163.com>
2017-04-09 21:34:05 +0800

    a
    a

---
+++ app/src/main/java/com/slayer/cxj/demoapp/MainActivity.java
@@ -0,0 +1,33 @@
+package com.slayer.cxj.demoapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.button_signup).setOnClickListener(this);                                  
+    }
+
+
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button_signup: {
+                Intent signUpIntent = new Intent(this, SignUpActivity.class);
+                startActivity(signUpIntent);
+                break;
+            }
+        }
+    }
+}
